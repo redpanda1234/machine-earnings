dae0eac9449dd9b0c8c98369650b93de0612bb2e
redpanda1234 <fkobayashi@hmc.edu>
2018-05-19 12:22:24 -0700

    fix import thing

--- src/scraper.py
+++ src/scraper.py
@@ -5,6 +5,8 @@ from alpha_vantage.timeseries import TimeSeries
 
 import api_keys
 
+alphavantage_key = api_keys.alphavantage_key
+
 ts = TimeSeries(key=alphavantage_key, output_format = "pandas")
 
 data = ts.get_intraday("GOOGL", interval = "60min", outputsize =
