5306d5816acea0e372363f68e9e22eca0a6fbf76
redpanda1234 <fkobayashi@hmc.edu>
2018-05-19 13:30:05 -0700

    do the windowed polynomial fitting

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
 
 import api_keys
@@ -9,14 +7,14 @@ alphavantage_key = api_keys.alphavantage_key
 
 ts = TimeSeries(key=alphavantage_key, output_format = "pandas")
 
-data = ts.get_intraday("GOOGL", interval = "60min", outputsize =
+data = ts.get_intraday("GOOGL", interval = "1min", outputsize =
                        "full")
 
 # data is a tuple of two values
 # data[0] contains the pandas dataframe
 # data[1] contains the dictionary of information about the data
 
-np_array = data[0].values
+data_array = data[0].values
 
 # each element x in np_array is an array with
 # x[0] : open
@@ -24,3 +22,19 @@ np_array = data[0].values
 # x[2] : low
 # x[3] : close
 # x[4] : volume
+
+# Size of windows we'll use to split the data into subintervals
+windows_size = 7
+
+# Get average price over each interval
+avg_arr = np.array([(x[1] + x[2])/2 for x in data_array])
+
+# Find out how many extraneous data points we have n
+end_ind = avg_arr.shape[0] % windows_size
+
+# Remove extra data so that the windows can be equally-sized
+avg_arr = avg_arr[:-end_ind]
+
+# Get the windows
+windows = np.array(np.split(avg_arr, avg_arr.shape[0] //\
+                            windows_size))
